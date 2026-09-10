af164c777a1bdb10c23d82d71c9b909377e3f61f
mogciaclient-ui <mogcia.client@gmail.com>
2026-09-10 15:56:37 +0900

    Add ismo cat mascot experiences

--- components/BrandLoader.tsx
+++ components/BrandLoader.tsx
@@ -7,7 +7,7 @@ export function BrandLoader({ label = "データを読み込んでいます", fu
       <span className="ismo-loader-orbit orbit-two" />
       <span className="ismo-loader-spark spark-one" />
       <span className="ismo-loader-spark spark-two" />
-      <div className="ismo-loader-logo"><Image src="/ismo-symbol.png" width={52} height={52} alt="" priority={fullPage} /></div>
+      <div className="ismo-loader-mascot"><Image src="/mascots/ismo-cat-loader.png" width={96} height={96} alt="" priority={fullPage} /></div>
     </div>
     <div className="ismo-loader-copy"><strong>SEE WHAT OTHERS MISS<span>.</span></strong><small>{label}</small></div>
   </div>;
